c2d529c5c665ca863c08d3fd63d943fea942f199
jajang_mahrul <jajangmahrul22@gmail.com>
2024-03-30 08:47:59 +0700

    Add Laravel Sanctum package and API routes

---
+++ app/Http/Controllers/Api/PostController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\PostResource;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::latest()->paginate(5);
+
+        return new PostResource(
+            true,
+            'List of posts',
+            $posts,
+        );
+    }
+}
