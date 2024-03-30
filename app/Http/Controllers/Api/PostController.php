f717d4864173149d99c93699a4c04e02d65c5575
jajang_mahrul <jajangmahrul22@gmail.com>
2024-03-30 08:58:28 +0700

    Add store method to PostController

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -2,10 +2,11 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
-use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\PostResource;
+use Illuminate\Support\Facades\Validator;
 
 class PostController extends Controller
 {
@@ -17,6 +18,34 @@ class PostController extends Controller
             true,
             'List of posts',
             $posts,
+        );
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'title' => 'required|string|max:255',
+            'content' => 'required|string',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        $image = $request->file('image');
+        $image->storeAs('public/posts', $image->hashName());
+
+        $post = Post::create([
+            'image' => $image->hashName(),
+            'title' => $request->title,
+            'content' => $request->content,
+        ]);
+
+        return new PostResource(
+            true,
+            'Post created',
+            $post,
         );
     }
 }
